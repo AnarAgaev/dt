e030bb7c5fd91cb9708ba9627a80735b4113aca1
Anar Agaev <anar.n.agaev@gmail.com>
2020-06-01 19:49:03 +0300

    added articl page

--- articles/index.php
+++ articles/index.php
@@ -1,25 +1,25 @@
 <? require_once '../template/prolog/all-pages.php'; ?>
+<? require_once '../template/prolog/articles-page.php'; ?>
 <!DOCTYPE html>
 <html lang="ru" xmlns:og="http://ogp.me/ns#">
 
 <head>
   <? require_once SITE__DIR.'template/components/head.php'; ?>
 
-  <?      // !!! Динамический татйтл !!!     ?>
-  <title>Designtalk - это БЛОГ О ДИЗАЙНЕ ПРОСТРАНСТВА.</title>
-  <meta name="description" content="" />
-  <meta name="keywords" content="" />
-  <link rel="canonical" href="https://designtalk.ru/articles/" } />
+  <title><?= $response['title']; ?> | Designtalk - это БЛОГ О ДИЗАЙНЕ ПРОСТРАНСТВА.</title>
+  <meta name="description" content="<?= $response['description']; ?>" />
+  <meta name="keywords" content="<?= $response['keywords']; ?>" />
+  <link rel="canonical" href="https://designtalk.ru/articles/<?= $response['url']; ?>" } />
 
-  <meta name="og:title" content="" />
-  <meta property="og:title" content="" />
-  <meta name="og:description" content="" />
-  <meta property="og:description" content="" />
-  <meta name="og:image" content="" />
-  <meta property="og:image" content="" />
+  <meta name="og:title" content="<?= $response['title']; ?>" />
+  <meta property="og:title" content="<?= $response['title']; ?>" />
+  <meta name="og:description" content="<?= $response['description']; ?>" />
+  <meta property="og:description" content="<?= $response['description']; ?>" />
+  <meta name="og:image" content="https://designtalk.ru/images/<?= $response['picture']; ?>" />
+  <meta property="og:image" content="https://designtalk.ru/images/<?= $response['picture']; ?>" />
   <meta property="og:type" content="article" />
-  <meta name="og:url" content="https://designtalk.ru/articles/" />
-  <meta property="og:url" content="https://designtalk.ru/articles/" />
+  <meta name="og:url" content="https://designtalk.ru/articles/<?= $response['url']; ?>" />
+  <meta property="og:url" content="https://designtalk.ru/articles/<?= $response['url']; ?>" />
   <meta name="twitter:card" content="summary_large_image" />
 </head>
 
@@ -28,8 +28,6 @@
   <? require_once SITE__DIR.'template/components/header.php'; ?>
 
   <main class="main">
-
-
     <div class="article container">
       <div class="row">
         <div class="col-lg-12">
@@ -37,99 +35,40 @@
         </div>
 
         <div class="col-lg-12">
-          <a class="article__rubric" href="/rubrics/malenkie-kvartiry">маленькие квартиры</a>
-          <h1 class="article__title">Брутальная квартира с графичными деталями, 52 кв. метра</h1>
-          <div class="article__subtitle">Дизайнеру Анне Гуцаловой позвонила знакомая и сказала: “Аня, моей подруге очень нужна помощь!”</div>
+          <a class="article__rubric" href="/rubrics/<?= $response['rubric']['link']; ?>"><?= $response['rubric']['name']; ?></a>
+          <h1 class="article__title"><?= $response['title']; ?></h1>
+          <div class="article__subtitle"><?= $response['subtitle']; ?></div>
           <div class="article__copy">
-            <span class="article__copywriter">юлия лусенкова</span>
-            <span class="article__date">21 АПРЕЛЯ 2020</span>
+            <span class="article__copywriter"><?= $response['copywriter']['name']; ?></span>
+            <span class="article__date"><?= $response['date']; ?></span>
           </div>
-          <img src="https://designtalk.ru/images/w988.jpg" alt="Брутальная квартира с графичными деталями, 52 кв. метра">
+          <img src="/images/<?= $response['picture']; ?>" alt="Брутальная квартира с графичными деталями, 52 кв. метра">
         </div>
 
         <div class="article__wrap col-lg-8">
           <div class="article__content">
-            <p> Хозяйка квартиры в ЖК TriBeCa Apartments сперва начала делать ремонт сама, но быстро поняла, что без помощи
-              профессионала ей не обойтись. Анна пришла посмотреть, с чем ей предстоит работать, и сразу оценила потолки
-              высотой четыре метра и панорамное окно, откуда открывался потрясающий вид на город и даже на две
-              высотки. </p>
-            <p> Заказчица уже видела несколько проектов Гуцаловой, сурово минималистичных, и попросила: “Аня, давай, пожалуйста,
-              сделаем так же брутально”. Дизайнер ее поддержала: “У здания брутальная архитектура, и здесь был нужен такой же
-              интерьер”.
-            </p>
-            <figure classname="sign" data-position="vertical">
-              <p> <img src="https://designtalk.ru/images/w658.jpg"
-                  alt="Зона столовой. Стеллаж сделан на заказ по эскизам дизайнера; винтажные стулья, Repeat Story; стол, Calligaris; над столом винтажная люстра из шоурума “Свет и не только 50–60–70‑х годов”; керамика, Davceramics; картина Сергея Филатова."
-                  title="Зона столовой. Стеллаж сделан на заказ по эскизам дизайнера; винтажные стулья, Repeat Story; стол, Calligaris; над столом винтажная люстра из шоурума “Свет и не только 50–60–70‑х годов”; керамика, Davceramics; картина Сергея Филатова.">
-              </p>
-              <figcaption>Зона столовой. Стеллаж сделан на заказ по эскизам дизайнера; винтажные стулья, Repeat Story; стол,
-                Calligaris; над столом винтажная люстра из шоурума “Свет и не только 50–60–70‑х годов”; керамика,
-                Davceramics; картина Сергея Филатова.</figcaption>
-            </figure>
-            <p> Цветовое решение определилось благодаря двум винтажным люстрам, сделанным в виде больших белых тарелок с
-              красными, желтыми и синими плафонами, которые хозяйка купила у своей приятельницы. “Это прямая отсылка к
-              Мондриану, и эту тему я решила продолжить по всей квартире — придумала графичную цветную нишу в шкафу в прихожей,
-              асимметричную раскладку окна в спальне и грубый металлический стеллаж в гостиной с необычной геометрией полок”, —
-              делится дизайнер. </p>
-            <figure classname="sign" data-position="vertical">
-              <p> <img src="https://designtalk.ru/images/w658sdf.jpg"
-                  alt="Кухонный гарнитур сделан на заказ по эскизам дизайнера. Столешница мраморная, а фартук комбинированный — часть отделана мозаикой, а часть зеркалом."
-                  title="Кухонный гарнитур сделан на заказ по эскизам дизайнера. Столешница мраморная, а фартук комбинированный — часть отделана мозаикой, а часть зеркалом.">
-              </p>
-              <figcaption>Кухонный гарнитур сделан на заказ по эскизам дизайнера. Столешница мраморная, а фартук комбинированный
-                — часть отделана мозаикой, а часть зеркалом.</figcaption>
-            </figure>
-            <p> Несмотря на то что большую часть времени заказчица живет в Санкт-Петербурге, свою московскую квартиру она
-              попросила сделать очень функциональной. Для того чтобы на небольшой площади поместилось все, прямо посередине
-              помещения Анна выстроила деревянный куб, в котором разместила спальню. С внешней стороны куба одну из стен
-              дизайнер превратила в часть кухни — здесь встроенные холодильник, винный шкаф и ящики для хранения. Рабочая часть
-              кухни с небольшой барной стойкой находится напротив. </p>
-            <figure classname="sign" data-position="vertical">
-              <p> <img src="https://designtalk.ru/images/w658gwsedf.jpg"
-                  alt="Спальня. Кровать сделана на заказ по эскизам дизайнера; покрывало и плед, все Togas; подушки, Missoni Home; бра из шоурума “Свет и не только 50–60–70‑х годов”; аксессуары, Happy Collections; картина Софьи Пятницкой из шоурума Repeat Story."
-                  title="Спальня. Кровать сделана на заказ по эскизам дизайнера; покрывало и плед, все Togas; подушки, Missoni Home; бра из шоурума “Свет и не только 50–60–70‑х годов”; аксессуары, Happy Collections; картина Софьи Пятницкой из шоурума Repeat Story.">
-              </p>
-              <figcaption>Спальня. Кровать сделана на заказ по эскизам дизайнера; покрывало и плед, все Togas; подушки, Missoni
-                Home; бра из шоурума “Свет и не только 50–60–70‑х годов”; аксессуары, Happy Collections; картина Софьи
-                Пятницкой из шоурума Repeat Story.</figcaption>
-            </figure>
-            <p> Анна захотела подчеркнуть одно из главных преимуществ этой квартиры — вид из панорамного окна, для этого она
-              воспользовалась цветом. “Когда вокруг окна стена черного цвета, получается эффект, будто ты смотришь на картину в
-              галерее — из квартиры можно наблюдать потрясающие закаты”, — делится секретом дизайнер. </p>
-            <figure classname="sign" data-position="vertical">
-              <p> <img src="https://designtalk.ru/images/w658ghfa.jpg"
-                  alt="Тумба под раковину, зеркало и душевая перегородка сделаны на заказ по эскизам дизайнера; одно из отделений тумбы занимает холодильник для косметики; винтажные бра из шоурума “Свет и не только 50–60–70‑х годов”."
-                  title="Тумба под раковину, зеркало и душевая перегородка сделаны на заказ по эскизам дизайнера; одно из отделений тумбы занимает холодильник для косметики; винтажные бра из шоурума “Свет и не только 50–60–70‑х годов”.">
-              </p>
-              <figcaption>Тумба под раковину, зеркало и душевая перегородка сделаны на заказ по эскизам дизайнера; одно из
-                отделений тумбы занимает холодильник для косметики; винтажные бра из шоурума “Свет и не только 50–60–70‑х
-                годов”.</figcaption>
-            </figure>
-            <p> А вот в спальне цвет стен выбрала хозяйка. “Она сказала: “Давай, может, сделаем черную?” — на что я ответила:
-              “Конечно, давай!!!” — вспоминает Анна Гуцалова. </p>
-            <figure classname="sign" data-position="vertical">
-              <p> <img src="https://designtalk.ru/images/w658hgasd.jpg"
-                  alt="Прихожая. Слева большой шкаф для верхней одежды и обуви, открытая его часть сделана в стилистике картин Пита Мондриана. Винтажная люстра, задавшая цвет и настроение всему интерьеру, из шоурума “Свет и не только 50–60–70-х годов”."
-                  title="Прихожая. Слева большой шкаф для верхней одежды и обуви, открытая его часть сделана в стилистике картин Пита Мондриана. Винтажная люстра, задавшая цвет и настроение всему интерьеру, из шоурума “Свет и не только 50–60–70-х годов”.">
-              </p>
-              <figcaption>Прихожая. Слева большой шкаф для верхней одежды и обуви, открытая его часть сделана в стилистике
-                картин Пита Мондриана. Винтажная люстра, задавшая цвет и настроение всему интерьеру, из шоурума “Свет и не
-                только 50–60–70-х годов”.</figcaption>
-            </figure>
-            <p> Работа над квартирой проходила не без происшествий, почти в конце ремонта квартиру затопило, пришлось
-              переделывать кухню и перекладывать пол из мелкого паркета, но это не убило настрой девушек. Анна говорит: “У нас с
-              Марией сложился идеальный тандем, мы сполна насладились и процессом, и результатом”. </p>
-            <figure classname="sign">
-              <p> <img src="https://designtalk.ru/images/w988hhads.png" alt="" title=""> </p>
-              <figcaption></figcaption>
-            </figure>
+            <?= $response['content']; ?>
           </div>
 
           <ul class="article-creators__list">
-            <li class="article-creators__item"><span class="article-creators__caption">автор проекта:</span><span
-                class="article-creators__person">анна гуцалова</span></li>
-            <li class="article-creators__item"><span class="article-creators__caption">стилист:</span><span
-                class="article-creators__person">виктория свиридова</span></li>
+            <? if ($response['author']['id']):   ?>
+              <li class="article-creators__item">
+                <span class="article-creators__caption">автор проекта:</span><span
+                class="article-creators__person"><?= $response['author']['name']; ?></span>
+              </li>
+            <? endif; ?>
+            <? if ($response['photographer']['id']):   ?>
+              <li class="article-creators__item">
+                <span class="article-creators__caption">фотограф:</span><span
+                class="article-creators__person"><?= $response['photographer']['name']; ?></span>
+              </li>
+            <? endif; ?>
+            <? if ($response['stylist']['id']):   ?>
+              <li class="article-creators__item">
+                <span class="article-creators__caption">стилист:</span><span
+                class="article-creators__person"><?= $response['stylist']['name']; ?></span>
+              </li>
+            <? endif; ?>
           </ul>
 
           <span class="sharing-article__caption">Читайте нас там где Вам удобно:</span>
@@ -146,7 +85,6 @@
 
      <? require_once SITE__DIR.'template/components/subscribe.php'; ?>
      <? require_once SITE__DIR.'template/components/last-rubric-article-list.php'; ?>
-
   </main>
 
   <? require_once SITE__DIR.'template/components/footer.php'; ?>
---
+++ template/prolog/articles-page.php
@@ -0,0 +1,165 @@
+<?
+
+  /*
+   * Getting data of the selected article from DB
+   */
+
+  // Parsing the URL of page and getting article name from it
+  $article_url = explode("/", $CLIENT__URL)[4];
+
+  $article = mysqli_fetch_assoc(mysqli_query($link, "
+    SELECT *
+    FROM `articles`
+    WHERE `url`='$article_url'
+  "));
+
+  // Getting rubric of article
+  $rubric = mysqli_fetch_assoc(mysqli_query($link, "
+    SELECT *
+    FROM `rubrics`
+    WHERE `id`='$article[rubric]'
+  " ));
+  $response['rubric'] = array (
+      'id' => $rubric['id'],
+      'link' => $rubric['link'],
+      'name' => $rubric['name']
+  );
+
+  // Getting copywriter of article
+  $copywriter = mysqli_fetch_assoc(mysqli_query($link, "
+    SELECT *
+    FROM `copywriters`
+    WHERE `id`='$article[copywriter]'
+  " ));
+  $response['copywriter'] = array (
+      'id' => $copywriter['id'],
+      'name' => $copywriter['name'],
+      'photo' => $copywriter['photo']
+  );
+
+  // Getting author of article
+  $author = mysqli_fetch_assoc(mysqli_query($link, "
+    SELECT *
+    FROM `authors`
+    WHERE `id`='$article[author]'
+  " ));
+  $response['author'] = array (
+      'id' => $author['id'],
+      'name' => $author['name'],
+      'photo' => $author['photo']
+  );
+
+  // Getting photographer of article
+  $photographer = mysqli_fetch_assoc(mysqli_query($link, "
+    SELECT *
+    FROM `photographers`
+    WHERE `id`='$article[photographer]'
+  " ));
+  $response['photographer'] = array (
+      'id' => $photographer['id'],
+      'name' => $photographer['name'],
+      'photo' => $photographer['photo']
+  );
+
+  // Getting stylist of article
+  $stylist = mysqli_fetch_assoc(mysqli_query($link, "
+    SELECT *
+    FROM `stylists`
+    WHERE `id`='$article[stylist]'
+  " ));
+  $response['stylist'] = array (
+      'id' => $stylist['id'],
+      'name' => $stylist['name'],
+      'photo' => $stylist['photo']
+  );
+
+  // date normalizer
+  require_once '../utils/normalize-date.php';
+  $response['date'] = normalize_date($article['date']);
+
+  // Getting other article data
+  $response['id'] = $article['id'];
+  $response['url'] = $article['url'];
+  $response['title'] = $article['title'];
+  $response['subtitle'] = $article['subtitle'];
+  $response['preview'] = $article['preview'];
+  $response['picture'] = $article['picture'];
+  $response['keywords'] = $article['keywords'];
+  $response['description'] = $article['description'];
+  $response['content'] = $article['content'];
+
+
+  // If the requested article is not there, rewrite the response to false
+  if ($response['id'] === null) {
+    $response = false;
+  } else {
+
+    // Update the number of article views in the database
+    mysqli_query($link, "
+      UPDATE `visits`
+      SET `visits`=`visits` + 1
+      WHERE `article`='$response[id]'
+    ");
+  }
+
+
+
+
+
+
+  /*
+   * Getting most popular four articles from
+   * the rubric of the selected article
+   */
+
+  $rubric_id = $response['rubric']['id'];
+
+  // Fetching rubric data
+  $rubric_data = mysqli_fetch_assoc(mysqli_query($link, "
+    SELECT `id`, `link`, `name`
+    FROM `rubrics`
+    WHERE `id`='$rubric_id'
+  "));
+
+  // Fetching most popular articles from the rubric
+  $article_list = mysqli_query($link, "
+    SELECT `article`, `rubric`, `visits`
+    FROM `visits`
+    WHERE `rubric`='$rubric_data[id]'
+    ORDER BY `visits` DESC
+    LIMIT 4
+  " );
+
+  if (mysqli_num_rows($article_list)) {
+
+    for($i = 0; $i < mysqli_num_rows($article_list); $i++) {
+      $row = mysqli_fetch_row($article_list);
+
+
+      // Fetching article data
+      $article = mysqli_fetch_assoc(mysqli_query($link, "
+        SELECT `id`, `url`, `title`, `preview`, `picture`
+        FROM `articles`
+        WHERE `id`='$row[0]'
+      "));
+
+
+      // Building the article and push it at the response array
+      $response['popular'][] = array(
+        'id' => $article['id'],
+        'url' => $article['url'],
+        'title' => $article['title'],
+        'preview' => $article['preview'],
+        'picture' => $article['picture'],
+        'rubric' => array(
+          'id' => $rubric_data['id'],
+          'link' => $rubric_data['link'],
+          'name' => $rubric_data['name']
+        ),
+        'visits' => $row[2],
+        'visible' => false
+      );
+    }
+  } else $response['popular'] = null;
+
+?>
